189253508b6d67a20ce5f6889f2a62fe2a0a9acb
SaltyFrappuccino <saltyfrappuccino@internet.ru>
2025-06-03 02:43:58 +0400

    1

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 # api.py
 from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from typing import List, Optional, Dict, Any
 import sqlite3
